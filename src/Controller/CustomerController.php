1ab4f6c0dcb1c9b36e134fe4fe38f43782b6babc
rerich3 <miroslav.imris@gmail.com>
2019-11-21 13:20:19 +0100

    domaciukol

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CustomerController extends AbstractController
+{
+    /**
+     * @Route("/customer/detail/id", name="id", methods="GET"))
+     */
+    public function detail()
+    {return $this->render('templates/customer/index.html.twig', ['controller_name' => 'Customercontroller',]);}
